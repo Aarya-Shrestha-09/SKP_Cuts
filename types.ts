2ef1db72a440911399e01bbcf2dff394f23d2b2a
Aarya_S <92388271+Aarya-Shrestha-09@users.noreply.github.com>
2026-01-03 17:18:45 -0600

    Add BookingFormData and GalleryImage interfaces

---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface BookingFormData {
+  name: string;
+  phone: string;
+  haircutType: string;
+  notes: string;
+}
+
+export interface GalleryImage {
+  url: string;
+  alt: string;
+}
